b5b381e8e21c7c593a013db531bf64f69f6eab6e
Joseph Lee <josephl@worldconcern.org>
2016-03-01 03:48:07 -0800

    Updating order of footer

--- footer.php
+++ footer.php
@@ -1,12 +1,12 @@
 					<footer class="footer" role="contentinfo">
 						<div id="inner-footer" class="row">
-							<div class="large-12 medium-12 columns">
-								<nav role="navigation">
-		    						<?php joints_footer_links(); ?>
-		    					</nav>
-		    				</div>
-							<div class="large-12 medium-12 columns">
+							<div class="large-6 medium-12 columns">
 								<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
+	    				</div>
+							<div class="large-6 medium-12 columns">
+								<nav role="navigation">
+									<?php joints_footer_links(); ?>
+								</nav>
 							</div>
 						</div> <!-- end #inner-footer -->
 					</footer> <!-- end .footer -->
