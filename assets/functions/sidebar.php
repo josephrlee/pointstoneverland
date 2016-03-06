5acc78dd455d3b2ca13bbe52b8b432ad4d719234
Joseph Lee <josephl@worldconcern.org>
2016-03-05 20:42:02 -0800

    Renaming Sidebar functions

--- assets/functions/sidebar.php
+++ assets/functions/sidebar.php
@@ -2,8 +2,8 @@
 // SIDEBARS AND WIDGETIZED AREAS
 function joints_register_sidebars() {
 	register_sidebar(array(
-		'id' => 'sidebar1',
-		'name' => __('Sidebar 1', 'jointswp'),
+		'id' => 'sidebar',
+		'name' => __('Sidebar', 'jointswp'),
 		'description' => __('The first (primary) sidebar.', 'jointswp'),
 		'before_widget' => '<div id="%1$s" class="widget %2$s">',
 		'after_widget' => '</div>',
