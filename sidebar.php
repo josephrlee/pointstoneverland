148054d2d5abdf33de5cfcf82dfc06fe814bc03a
Joseph Lee <josephl@worldconcern.org>
2016-03-05 20:41:28 -0800

    Adding Top 10 to sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,13 +1,15 @@
-<div id="sidebar1" class="sidebar large-4 medium-4 columns" role="complementary">
+<div id="sidebar" class="sidebar large-4 medium-4 columns" role="complementary">
 
-	<?php if ( is_active_sidebar( 'sidebar1' ) ) : ?>
+	<?php if ( is_active_sidebar( 'sidebar' ) ) : ?>
 
-		<?php dynamic_sidebar( 'sidebar1' ); ?>
-
+		<?php dynamic_sidebar( 'sidebar' ); ?>
+		<?php $id = 9; $p = get_page($id); echo apply_filters('the_content', $p->post_content); ?>
+		
 	<?php else : ?>
 
+
 	<!-- This content shows up if there are no widgets defined in the backend. -->
-						
+
 	<div class="alert help">
 		<p><?php _e( 'Please activate some Widgets.', 'jointswp' );  ?></p>
 	</div>
