b4ea7789e0399506e18f95df2eb1f04b8c5ed748
Joseph Lee <josephl@worldconcern.org>
2016-03-07 00:30:57 -0800

    Formatting Top 10 in the sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,17 +1,19 @@
 <div id="sidebar" class="sidebar large-4 medium-4 columns" role="complementary">
 
-	<?php if ( is_active_sidebar( 'sidebar' ) ) : ?>
+	<?php if (is_active_sidebar('sidebar')) : ?>
 
-		<?php dynamic_sidebar( 'sidebar' ); ?>
-		<?php $id = 9; $p = get_page($id); echo apply_filters('the_content', $p->post_content); ?>
-		
+		<?php dynamic_sidebar('sidebar'); ?>
+		<aside class="top-10">
+			<h3>Top 10</h3>
+			<?php $id = 9; $p = get_page($id); echo apply_filters('the_content', $p->post_content); ?>
+		</aside>
 	<?php else : ?>
 
 
 	<!-- This content shows up if there are no widgets defined in the backend. -->
 
 	<div class="alert help">
-		<p><?php _e( 'Please activate some Widgets.', 'jointswp' );  ?></p>
+		<p><?php _e('Please activate some Widgets.', 'jointswp');  ?></p>
 	</div>
 
 	<?php endif; ?>
