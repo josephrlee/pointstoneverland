82aa07b1f5531e544b94654d0570f535ee0d50db
Joseph Lee <josephl@worldconcern.org>
2016-03-12 17:34:20 -0800

    Added search form to off-canvas menu

--- parts/content-offcanvas.php
+++ parts/content-offcanvas.php
@@ -1,3 +1,9 @@
-<div class="off-canvas position-right" id="off-canvas" data-off-canvas data-position="right">
+<aside class="off-canvas position-right" id="off-canvas" data-off-canvas data-position="right">
+	<form role="search" method="get" class="search-form input-group" action="<?php echo home_url('/'); ?>">
+		<input type="search" class="search-field input-group-field" placeholder="<?php echo esc_attr_x('Search...', 'jointswp') ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x('Search for:', 'jointswp') ?>" />
+		<div class="input-group-button">
+			<input type="submit" class="search-submit button " value="<?php echo esc_attr_x('Search', 'jointswp') ?>" />
+		</div>
+	</form>
 	<?php joints_off_canvas_nav(); ?>
-</div>
+</aside>
