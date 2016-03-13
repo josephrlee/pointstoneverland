cb958700755caacee3be0bbe8b2f2d8c82da58af
Joseph Lee <josephl@worldconcern.org>
2016-03-12 17:31:08 -0800

    Added logo and made top nav follow global width -Added .svg logo -Changed max-width behavior for .row -Refined top nav styles -Fixed top 10 sidebar display -Added row container to top bar

--- parts/nav-offcanvas.php
+++ parts/nav-offcanvas.php
@@ -1,15 +1,16 @@
-<div class="top-bar" id="top-bar-menu">
-	<div class="top-bar-left">
-		<ul class="menu">
-			<li><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></li>
-		</ul>
-	</div>
-	<div class="top-bar-right">
-
-		<ul class="menu">
-			<li class="primary-menu"><?php joints_top_nav(); ?></li>
-			<li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li>
-			<li><a data-toggle="off-canvas"><?php _e( 'Menu', 'jointswp' ); ?></a></li>
-		</ul>
-	</div>
-</div>
+<nav class="top-bar" id="top-bar-menu">
+	<section class="row">
+		<div class="top-bar-left">
+			<ul class="menu">
+				<li><a href="<?php echo home_url(); ?>" class="logo"><img src="<?php bloginfo('template_directory'); ?>/assets/images/logo.svg" alt="<?php bloginfo('name'); ?>" width="160" /></a></li>
+			</ul>
+		</div>
+		<div class="top-bar-right">
+			<ul class="menu">
+				<li class="primary-menu hide-for-medium-down" data-hide-for="medium"><?php joints_top_nav(); ?></li>
+				<li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li>
+				<li><a data-toggle="off-canvas"><?php _e('Menu', 'jointswp'); ?></a></li>
+			</ul>
+		</div>
+	</section>
+</nav>
