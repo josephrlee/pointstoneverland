091dd26823e4fb4ecdecd17b355ce95d263c1743
Joseph Lee <josephl@worldconcern.org>
2016-03-15 03:45:58 -0700

    Added hero banner image and adjusted header to respond properly -Added multiple versions of the banner for different screen sizes and densities -Added retina styles -Styled hero banner to respond properly -Styled primary nav -Hid primary nav for medium screens and down

--- parts/nav-offcanvas.php
+++ parts/nav-offcanvas.php
@@ -1,13 +1,11 @@
 <nav class="top-bar" id="top-bar-menu">
 	<section class="row">
 		<div class="top-bar-left">
-			<ul class="menu">
-				<li><a href="<?php echo home_url(); ?>" class="logo"><img src="<?php bloginfo('template_directory'); ?>/assets/images/logo.svg" alt="<?php bloginfo('name'); ?>" width="160" /></a></li>
-			</ul>
+			<a href="<?php echo home_url(); ?>" class="logo"><img src="<?php bloginfo('template_directory'); ?>/assets/images/logo.svg" alt="<?php bloginfo('name'); ?>" width="160" /></a>
 		</div>
 		<div class="top-bar-right">
 			<ul class="menu">
-				<li class="primary-menu hide-for-medium-down" data-hide-for="medium"><?php joints_top_nav(); ?></li>
+				<li class="primary-menu hide-for-medium-only" data-hide-for="large"><?php joints_top_nav(); ?></li>
 				<li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li>
 				<li><a data-toggle="off-canvas"><?php _e('Menu', 'jointswp'); ?></a></li>
 			</ul>
