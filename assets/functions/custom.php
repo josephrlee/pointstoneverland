7ef31b9bd0fc0636753a3766ee367d20da319829
Joseph Lee <josephl@worldconcern.org>
2016-03-19 23:11:55 -0700

    Added excerpt function to grab first paragraph of post and also custom length

--- assets/functions/custom.php
+++ assets/functions/custom.php
@@ -17,12 +17,58 @@ function highlight_search_term($text)
 add_filter('the_excerpt', 'highlight_search_term');
 add_filter('the_title', 'highlight_search_term');
 
-// Filter the except length to 20 characters.
-// @param int $length Excerpt length.
-// @return int (Maybe) modified excerpt length.
-
-function wpdocs_custom_excerpt_length($length)
-{
-  return 40;
+// Filter the except to custom length.
+function excerpt($limit) {
+ $excerpt = explode(' ', get_the_excerpt(), $limit);
+ if (count($excerpt)>=$limit) {
+ array_pop($excerpt);
+ $excerpt = implode(" ",$excerpt).'...';
+ } else {
+ $excerpt = implode(" ",$excerpt);
+ }
+ $excerpt = preg_replace('`[[^]]*]`','',$excerpt);
+ return $excerpt;
 }
-add_filter('excerpt_length', 'wpdocs_custom_excerpt_length', 999);
+
+function content($limit) {
+ $content = explode(' ', get_the_content(), $limit);
+ if (count($content)>=$limit) {
+ array_pop($content);
+ $content = implode(" ",$content).'...';
+ } else {
+ $content = implode(" ",$content);
+ }
+ $content = preg_replace('/[.+]/','', $content);
+ $content = apply_filters('the_content', $content);
+ $content = str_replace(']]>', ']]&gt;', $content);
+ return $content;
+}
+
+
+/**
+ *  Create a custom excerpt string from the first paragraph of the content.
+ *
+ *  @param   integer  $id       The id of the post
+ *  @return  string   $excerpt  The excerpt string
+ */
+function first_paragraph_excerpt( $id=null ) {
+    // Set $id to the current post by default
+    if( !$id ) {
+        global $post;
+        $id = get_the_id();
+    }
+    // Get the post content
+    $content = get_post_field( 'post_content', $id );
+    $content = apply_filters( 'the_content', strip_shortcodes( $content ) );
+    // Remove all tags, except paragraphs
+    $excerpt = strip_tags( $content, '<p></p>' );
+    // Remove empty paragraph tags
+    $excerpt = force_balance_tags( $excerpt );
+    $excerpt = preg_replace( '#<p>\s*+(<br\s*/*>)?\s*</p>#i', '', $excerpt );
+    $excerpt = preg_replace( '~\s?<p>(\s|&nbsp;)+</p>\s?~', '', $excerpt );
+    // Get the first paragraph
+    $excerpt = substr( $excerpt, 0, strpos( $excerpt, '</p>' ) + 4 );
+    // Remove remaining paragraph tags
+    $excerpt = strip_tags( $excerpt );
+    return $excerpt;
+}
