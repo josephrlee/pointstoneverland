16cce3f94b31cd39311f2e4322a7925687e43130
Joseph Lee <josephl@worldconcern.org>
2016-03-19 23:06:14 -0700

    Updated how posts in homepage display

--- parts/loop-archive.php
+++ parts/loop-archive.php
@@ -1,15 +1,10 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(''); ?> role="article">
-	<header class="article-header">
-		<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-		<?php get_template_part( 'parts/content', 'byline' ); ?>
-	</header> <!-- end article header -->
-
-	<section class="entry-content" itemprop="articleBody">
-		<a href="<?php the_permalink() ?>"><?php the_post_thumbnail('full'); ?></a>
-		<?php the_excerpt(); ?>
-	</section> <!-- end article section -->
-
-	<footer class="article-footer">
-    	<p class="tags"><?php the_tags('<span class="tags-title">' . __('Tags:', 'jointstheme') . '</span> ', ', ', ''); ?></p>
-	</footer> <!-- end article footer -->
+  <header class="article-header">
+    <h2 class="float-left"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+  </header> <!-- end article header -->
+  <section class="entry-content" itemprop="articleBody">
+    <a href="<?php the_permalink() ?>"><?php the_post_thumbnail('large');?></a>
+    <p><?php echo first_paragraph_excerpt(); ?></p>
+    <p><a href="<?php the_permalink() ?>" title="Continue Reading Post ?>" class="excerpt-read-more button">Read More</a></p>
+  </section> <!-- end article section -->
 </article> <!-- end article -->
