e834f065e9f4f74110fc56f4ab84760d3293444e
Joseph Lee <josephl@worldconcern.org>
2016-03-19 23:10:03 -0700

    Added content slider to theme -Added slider include to homepage -Styled slider so that image fills the height and covers it proportionally -Added custom slider part

--- index.php
+++ index.php
@@ -1,34 +1,31 @@
 <?php get_header(); ?>
 
-	<div id="content">
+<div id="content">
+  <div id="inner-content" class="row">
+      <section id="posts-slider" class="large-12 columns hide-for-small-only">
+        <?php get_template_part( 'parts/loop', 'slider' ); ?>
+      </section>
+      <main id="main" class="large-7 medium-6 columns" role="main">
+      <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <!-- To see additional archive styles, visit the /parts directory -->
+        <?php get_template_part( 'parts/loop', 'archive' ); ?>
 
-		<?php echo do_shortcode('[recent_post_slider limit="4" design="design-1" show_author="false" show_category_name="false" show_content="true" show_date="true" dots="true" arrows="true" autoplay="true" autoplay_interval="5000" speed="1000" content_words_limit="20"]');  ?>
+      <?php endwhile; ?>
 
-		<div id="inner-content" class="row">
+      <?php joints_page_navi(); ?>
 
-		    <main id="main" class="large-7 medium-6 columns" role="main">
+    <?php else : ?>
 
-			    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+      <?php get_template_part( 'parts/content', 'missing' ); ?>
+      <?php endif; ?>
 
-					<!-- To see additional archive styles, visit the /parts directory -->
-					<?php get_template_part( 'parts/loop', 'archive' ); ?>
+    </main> <!-- end #main -->
 
-				<?php endwhile; ?>
 
-					<?php joints_page_navi(); ?>
+  <?php get_sidebar(); ?>
 
-				<?php else : ?>
+</div> <!-- end #inner-content -->
 
-					<?php get_template_part( 'parts/content', 'missing' ); ?>
-
-				<?php endif; ?>
-
-		    </main> <!-- end #main -->
-
-		    <?php get_sidebar(); ?>
-
-		</div> <!-- end #inner-content -->
-
-	</div> <!-- end #content -->
+</div> <!-- end #content -->
 
 <?php get_footer(); ?>
---
+++ parts/loop-slider.php
@@ -0,0 +1,20 @@
+
+<div class="orbit" role="region" aria-label="Favorite Space Pictures" data-orbit>
+  <ul class="orbit-container">
+    <button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
+    <button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button>
+    <?php
+      $postslist = get_posts('numberposts=2&order=DESC&orderby=date');
+      foreach ($postslist as $post) :
+      setup_postdata($post);
+    ?>
+    <li class="orbit-slide">
+      <a href="<?php the_permalink() ?>"><?php the_post_thumbnail('full', array( 'class'	=> "orbit-image")); ?></a>
+      <figcaption class="orbit-caption large-4 columns large-push-8">
+        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+        <p class="lead"><?php echo excerpt(20); ?></p>
+      </figcaption>
+    </li>
+    <?php endforeach; ?>
+  </ul>
+</div>
