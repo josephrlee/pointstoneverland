16153c42ebe90645b7ff9d0939fa3d75da0364b7
Joseph Lee <josephl@worldconcern.org>
2016-03-24 01:50:24 -0700

    Updated Top 10 output for sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,21 +1,31 @@
 <div id="sidebar" class="sidebar large-5 medium-6 columns" role="complementary">
 
-	<?php if (is_active_sidebar('sidebar')) : ?>
+  <?php if (is_active_sidebar('sidebar')) : ?>
 
-		<!-- <aside class="top-10">
-			<h3>Top 10</h3>
-			<?php /* $id = 9; $p = get_page($id); echo apply_filters('the_content', $p->post_content); */?>
-		</aside> -->
-		<?php dynamic_sidebar('sidebar'); ?>
-	<?php else : ?>
+  <?php dynamic_sidebar('sidebar'); ?>
+  <aside class="top-10">
+    <h3>Top 10</h3>
+    <?php $id = 9; $p = get_page($id); echo apply_filters('the_content', $p->post_content); ?>
+    <?php if( have_rows('top_10_entry', 9) ): ?>
+    <ol class="top-10-list entry-content">
+      <?php while( have_rows('top_10_entry', 9) ): the_row();
+      // vars
+      $title = get_sub_field('title');
+      ?>
+      <li><?php echo $title; ?></li>
+    <?php endwhile; ?>
+    </ol>
+  <?php endif; ?>
+  </aside>
 
+<?php else : ?>
 
-	<!-- This content shows up if there are no widgets defined in the backend. -->
+  <!-- This content shows up if there are no widgets defined in the backend. -->
 
-	<div class="alert help">
-		<p><?php _e('Please activate some Widgets.', 'jointswp');  ?></p>
-	</div>
+  <div class="alert help">
+    <p><?php _e('Please activate some Widgets.', 'jointswp');  ?></p>
+  </div>
 
-	<?php endif; ?>
+<?php endif; ?>
 
 </div>
