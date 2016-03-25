e60f191c543663dbf2ab33da4ad1754f49b2dc5f
Joseph Lee <josephl@worldconcern.org>
2016-03-24 23:34:55 -0700

    Added disclaimer text to footer

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 					<footer class="footer" role="contentinfo">
 						<div id="inner-footer" class="row">
-							<div class="medium-6 columns">
-								<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
+							<div class="medium-8 columns">
+								<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. This site is not affiliated with The Walt Disney Company in any way.</p>
 	    				</div>
-							<div class="medium-6 columns">
+							<div class="medium-4 columns">
 								<nav role="navigation">
 									<?php joints_social_links(); ?>
                   <?php joints_footer_links(); ?>
