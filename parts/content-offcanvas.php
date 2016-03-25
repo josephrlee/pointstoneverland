c08dcf32d7e8def39d0d29ca3a7a802934088faf
Joseph Lee <josephl@worldconcern.org>
2016-03-24 23:34:15 -0700

    Making search form more uniform

--- parts/content-offcanvas.php
+++ parts/content-offcanvas.php
@@ -2,7 +2,7 @@
 	<form role="search" method="get" class="search-form input-group" action="<?php echo home_url('/'); ?>">
 		<input type="search" class="search-field input-group-field" placeholder="<?php echo esc_attr_x('Search...', 'jointswp') ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x('Search for:', 'jointswp') ?>" />
 		<div class="input-group-button">
-			<input type="submit" class="search-submit button " value="<?php echo esc_attr_x('Search', 'jointswp') ?>" />
+			<input type="submit" class="search-submit button input-group-field" value="<?php echo esc_attr_x('Search', 'jointswp') ?>" />
 		</div>
 	</form>
 	<?php joints_off_canvas_nav(); ?>
