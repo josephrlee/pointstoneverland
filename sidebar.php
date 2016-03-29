194f97655c3051d6e492150727df8a56f11c86b7
Joseph Lee <josephl@worldconcern.org>
2016-03-28 22:06:18 -0700

    Added Featured Card to Sidebar -Added featured credit card to sidebar -Added checkbox to Popular Cards page to define featured card -Styled featured card

--- sidebar.php
+++ sidebar.php
@@ -18,6 +18,23 @@
   <?php endif; ?>
   </aside>
 
+  <?php if( have_rows('credit_card_entry', 11) ): ?>
+  <aside class="featured-card">
+      <?php while( have_rows('credit_card_entry', 11) ): the_row();
+      // vars
+      $featured_card = get_sub_field('featured_card');
+      $card_name = get_sub_field('card_name');
+      $image = get_sub_field('card_image');
+      $url = get_sub_field('post_link');
+      ?>
+      <?php if($featured_card==1): ?>
+        <h3>Featured Card</h3>
+        <p><a href="<?php echo $url; ?>" title="Learn More"><img src="<?php echo $image['url']; ?>" alt="<<?php echo $image['alt'] ?>" /></a></p>
+      <?php endif; ?>
+    <?php endwhile; ?>
+  </aside>
+  <?php endif; ?>
+
 <?php else : ?>
 
   <!-- This content shows up if there are no widgets defined in the backend. -->
--- template-popular-cards.php
+++ template-popular-cards.php
@@ -40,6 +40,8 @@ Template Name: Popular Cards
                 $point_value = get_sub_field('point_value');
                 $spend_points = get_sub_field('how_to_spend_points');
                 $favorite_use = get_sub_field('our_favorite_use');
+                $url = get_sub_field('post_link');
+                $apply = get_sub_field('apply');
 
                 ?>
 
@@ -58,6 +60,8 @@ Template Name: Popular Cards
                       <li><strong>How to Spend Points</strong>: <?php echo $spend_points; ?></li>
                       <li><strong>Our favorite Use</strong>: <?php echo $favorite_use; ?></li>
                     </ul>
+                    <?php if( $url ): ?><p><a href="<?php echo $url; ?>" title="Read More" class="button">Read More</a></p><?php endif; ?>
+                    <?php if( $apply ): ?><p><a href="<?php echo $apply; ?>" title="Read More" class="button">Apply Now</a></p><?php endif; ?>
                   </td>
                 </tr>
               <?php endwhile; ?>
