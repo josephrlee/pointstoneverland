cc6a742e1e4a459cfe3467c69dedb21a56de63b6
Joseph Lee <josephl@worldconcern.org>
2016-04-04 23:39:06 -0700

    Added ability to add featured card to individual posts

--- parts/loop-single.php
+++ parts/loop-single.php
@@ -1,18 +1,60 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(''); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
-						
-	<header class="article-header">	
+
+	<header class="article-header">
 		<h1 class="entry-title single-title" itemprop="headline"><?php the_title(); ?></h1>
 		<?php get_template_part( 'parts/content', 'byline' ); ?>
     </header> <!-- end article header -->
-					
+
     <section class="entry-content" itemprop="articleBody">
 		<?php the_post_thumbnail('full'); ?>
 		<?php the_content(); ?>
 	</section> <!-- end article section -->
-						
+
 	<footer class="article-footer">
 		<p class="tags"><?php the_tags('<span class="tags-title">' . __( 'Tags:', 'jointswp' ) . '</span> ', ', ', ''); ?></p>	</footer> <!-- end article footer -->
-									
-	<?php comments_template(); ?>	
-													
+
+    <?php if( have_rows('credit_card_entry') ): ?>
+      <table class="card hover">
+        <tbody>
+          <?php while( have_rows('credit_card_entry') ): the_row();
+          // vars
+          $card_name = get_sub_field('card_name');
+          $image = get_sub_field('card_image');
+          $kind = get_sub_field('what_kind_of_points');
+          $bonus = get_sub_field('sign_up_bonus');
+          $fee = get_sub_field('annual_fee');
+          $earn_points = get_sub_field('how_to_earn_points');
+          $point_value = get_sub_field('point_value');
+          $spend_points = get_sub_field('how_to_spend_points');
+          $favorite_use = get_sub_field('our_favorite_use');
+          $url = get_sub_field('post_link');
+          $apply = get_sub_field('apply');
+
+          ?>
+
+          <tr>
+            <td class="card-name">
+              <h4><strong><?php echo $card_name; ?></strong></h4>
+              <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" />
+            </td>
+            <td class="card-details">
+              <ul>
+                <li><strong>What kind of points</strong>: <?php echo $kind; ?></li>
+                <li><strong>Sign Up Bonus</strong>: <?php echo $bonus; ?></li>
+                <li><strong>Annual Fee</strong>: $<?php echo $fee; ?></li>
+                <li><strong>How to Earn Points</strong>: <?php echo $earn_points; ?></li>
+                <li><strong>Value of a Point</strong>: <?php echo $point_value; ?> cents</li>
+                <li><strong>How to Spend Points</strong>: <?php echo $spend_points; ?></li>
+                <li><strong>Our favorite Use</strong>: <?php echo $favorite_use; ?></li>
+              </ul>
+              <?php if( $url ): ?><p><a href="<?php echo $url; ?>" title="Read More" class="button">Read More</a></p><?php endif; ?>
+              <?php if( $apply ): ?><p><a href="<?php echo $apply; ?>" title="Read More" class="button">Apply Now</a></p><?php endif; ?>
+            </td>
+          </tr>
+        <?php endwhile; ?>
+      </tbody>
+    </table>
+    <?php endif; ?>                  
+	<?php comments_template(); ?>
+
 </article> <!-- end article -->
