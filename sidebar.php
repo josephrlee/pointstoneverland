115d9ba5378afbd48c29ecfce03442c874c2ff0b
Joseph Lee <josephl@worldconcern.org>
2016-04-04 23:40:46 -0700

    Added search form to sidebar

--- sidebar.php
+++ sidebar.php
@@ -10,6 +10,7 @@
       <?php while( have_rows('top_10_entry', 9) ): the_row();
       // vars
       $title = get_sub_field('title');
+      $url = get_sub_field('post_url');
       ?>
       <li><?php echo $title; ?></li>
     <?php endwhile; ?>
@@ -35,6 +36,12 @@
   </aside>
   <?php endif; ?>
 
+  <form role="search" method="get" class="search-form input-group" action="<?php echo home_url('/'); ?>">
+  	<input type="search" class="search-field input-group-field" placeholder="<?php echo esc_attr_x('Search...', 'jointswp') ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x('Search for:', 'jointswp') ?>" />
+  	<div class="input-group-button">
+  		<input type="submit" class="search-submit button input-group-field" value="<?php echo esc_attr_x('Search', 'jointswp') ?>" />
+  	</div>
+  </form>
 <?php else : ?>
 
   <!-- This content shows up if there are no widgets defined in the backend. -->
