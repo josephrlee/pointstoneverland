e85dfa088820d0a1d7ea5b0c088c8ef082509530
Joseph Lee <josephl@worldconcern.org>
2016-04-05 21:54:25 -0700

    Changing name of homepage slider

--- parts/loop-slider.php
+++ parts/loop-slider.php
@@ -1,5 +1,5 @@
 
-<div class="orbit" role="region" aria-label="Favorite Space Pictures" data-orbit>
+<div class="orbit" role="region" aria-label="Featured Post" data-orbit>
   <ul class="orbit-container">
     <!-- <button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
     <button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button> -->
