7e4996e5ea1cd6350de496389d7b361d90a59196
Joseph Lee <josephl@worldconcern.org>
2016-04-05 22:40:53 -0700

    Added intro sentence to Top 10 sidebar aside

--- sidebar.php
+++ sidebar.php
@@ -4,6 +4,7 @@
   <?php dynamic_sidebar('sidebar'); ?>
   <aside class="top-10 medium-6 large-12 columns">
     <h3>Top 10</h3>
+    <p>Our favorite uses of credit card points at Disney World or Disneyland.</p>
     <?php if( have_rows('top_10_entry', 9) ): ?>
     <ol class="top-10-list entry-content">
       <?php while( have_rows('top_10_entry', 9) ): the_row();
