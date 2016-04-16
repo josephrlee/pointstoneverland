f1114b1ea9ea2ab1e98647add9482d219da6bc49
Joseph Lee <josephl@worldconcern.org>
2016-04-16 16:11:34 -0700

    Updating look and feel of Top 10 into cards.

--- template-top-10.php
+++ template-top-10.php
@@ -25,32 +25,30 @@ Template Name: Top 10
             <?php wp_link_pages(); ?>
           </section> <!-- end article section -->
 
-
           <?php if( have_rows('top_10_entry') ): ?>
-            <ol class="top-10-list entry-content">
-              <?php while( have_rows('top_10_entry') ): the_row();
-              // vars
-              $title = get_sub_field('title');
-              $what_it_takes = get_sub_field('what_it_takes');
-              $value = get_sub_field('value');
-              $image = get_sub_field('image');
-              $url = get_sub_field('post_url');
-              ?>
-              <li>
-                <h4><?php echo $title; ?><?php if( $image ): ?><img class="size-medium alignright" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" width="400"/><?php endif; ?></h4>
+            <?php while( have_rows('top_10_entry') ): the_row();
+            // vars
+            $title = get_sub_field('title');
+            $what_it_takes = get_sub_field('what_it_takes');
+            $value = get_sub_field('value');
+            $image = get_sub_field('image');
+            $url = get_sub_field('post_url');
+            ?>
+            <div class="callout">
+            <section class="top-10-list row">
+              <figure class="column medium-6 large-5 full">
+              <?php if( $image ): ?><img class="size-medium" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>"/><?php endif; ?>
+              </figure>
+              <main class="column medium-6 large-7">
+                <h3><?php echo $title; ?></h3>
                 <p><strong>What it takes</strong>: <?php echo $what_it_takes; ?></p>
                 <p><strong>Value</strong>: $<?php echo $value; ?></p>
                 <?php if( $url ): ?><p><a href="<?php echo $url; ?>" title="Read More" class="button">Read More</a></p><?php endif; ?>
-              </li>
-              <?php endwhile; ?>
-            </ol>
+              </main>
+            </section>
+          </div>
+            <?php endwhile; ?>
           <?php endif; ?>
-
-          <footer class="article-footer">
-
-          </footer> <!-- end article footer -->
-
-          <?php comments_template(); ?>
 
         </article> <!-- end article -->
 
