617bd805f2924ce3f63f567b1b3f6016cf4cc128
Joseph Lee <josephl@worldconcern.org>
2016-04-20 18:31:10 -0700

    Added number counter and styles to Top 10 list

--- template-top-10.php
+++ template-top-10.php
@@ -25,8 +25,9 @@ Template Name: Top 10
             <?php wp_link_pages(); ?>
           </section> <!-- end article section -->
 
-          <?php if( have_rows('top_10_entry') ): ?>
-            <?php while( have_rows('top_10_entry') ): the_row();
+          <?php if (have_rows('top_10_entry')): ?>
+            <?php $ctr = 1; ?>
+            <?php while (have_rows('top_10_entry')): the_row();
             // vars
             $title = get_sub_field('title');
             $what_it_takes = get_sub_field('what_it_takes');
@@ -35,18 +36,20 @@ Template Name: Top 10
             $url = get_sub_field('post_url');
             ?>
             <div class="callout">
-            <section class="top-10-list row">
-              <figure class="column medium-6 large-5 full">
-              <?php if( $image ): ?><img class="size-medium" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>"/><?php endif; ?>
-              </figure>
-              <main class="column medium-6 large-7">
-                <h3><?php echo $title; ?></h3>
-                <p><strong>What it takes</strong>: <?php echo $what_it_takes; ?></p>
-                <p><strong>Value</strong>: $<?php echo $value; ?></p>
-                <?php if( $url ): ?><p><a href="<?php echo $url; ?>" title="Read More" class="button">Read More</a></p><?php endif; ?>
-              </main>
-            </section>
-          </div>
+              <section class="top-10-list row">
+                <figure class="column medium-6 large-5 full">
+                  <figcaption><?php echo $ctr; ?></figcaption>
+                  <?php if ($image): ?><img class="size-medium" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>"/><?php endif; ?>
+                </figure>
+                <main class="column medium-6 large-7">
+                  <h3><?php echo $title; ?></h3>
+                  <p><strong>What it takes</strong>: <?php echo $what_it_takes; ?></p>
+                  <p><strong>Value</strong>: $<?php echo $value; ?></p>
+                  <?php if ($url): ?><p><a href="<?php echo $url; ?>" title="Read More" class="button">Read More</a></p><?php endif; ?>
+                </main>
+              </section>
+            </div>
+            <?php ++$ctr; ?>
             <?php endwhile; ?>
           <?php endif; ?>
 
