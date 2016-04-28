3687b89792e75862f99f4f2906524468b070fdc0
Joseph Lee <josephl@worldconcern.org>
2016-04-28 13:18:57 -0700

    Added close button to off-canvas nav

--- parts/content-offcanvas.php
+++ parts/content-offcanvas.php
@@ -1,4 +1,8 @@
 <aside class="off-canvas position-right" id="off-canvas" data-off-canvas data-position="right">
+  <!-- Close button -->
+  <button class="close-button" aria-label="Close menu" type="button" data-close>
+    <span aria-hidden="true">&times;</span>
+  </button>
 	<form role="search" method="get" class="search-form input-group" action="<?php echo home_url('/'); ?>">
 		<input type="search" class="search-field input-group-field" placeholder="<?php echo esc_attr_x('Search...', 'jointswp') ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x('Search for:', 'jointswp') ?>" />
 		<div class="input-group-button">
