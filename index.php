4e983ff34a690d79977dff401f2ea2774308a97b
Joseph Lee <josephl@worldconcern.org>
2016-05-13 16:10:31 -0700

    Fixed pagination and offset bug
    * Removed page offset on posts
    * Changed index to use if/then statement for first post to display slider correctly
    * Formatted code

--- index.php
+++ index.php
@@ -4,29 +4,52 @@
 
   <div id="inner-content" class="row">
 
+    <?php if (have_posts()) : ?>
+    <?php $post = $posts[0]; $c = 0;?>
+
+    <?php the_post(); ++$c; ?>
+    <?php if (!$paged && $c == 1) :?>
+    <!-- Index Page -->
     <section id="posts-slider" class="large-12 columns">
-      <?php get_template_part( 'parts/loop', 'slider' ); ?>
+      <?php get_template_part('parts/loop', 'slider'); ?>
     </section>
 
     <main id="main" class="large-8 columns" role="main">
-      <?php query_posts('posts_per_page=6&offset=1'); if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+      <?php while (have_posts()) : the_post(); ?>
       <!-- To see additional archive styles, visit the /parts directory -->
-        <?php get_template_part( 'parts/loop', 'archive' ); ?>
+      <?php get_template_part('parts/loop', 'archive'); ?>
 
       <?php endwhile; ?>
 
       <?php joints_page_navi(); ?>
 
-      <?php else : ?>
-        <?php get_template_part( 'parts/content', 'missing' ); ?>
-      <?php endif; ?>
+    </main>
+    <?php endif; //end of if (!$paged && $c==1 ) ?>
 
-    </main> <!-- end #main -->
+    <?php ++$c; if ($paged != 1 && $c != 1) :?>
+    <!-- Secondary pages -->
+    <main id="main" class="large-8 columns" role="main">
+
+      <?php while (have_posts()) : the_post(); ?>
+      <!-- To see additional archive styles, visit the /parts directory -->
+      <?php get_template_part('parts/loop', 'archive'); ?>
+
+      <?php endwhile; ?>
+
+      <?php joints_page_navi(); ?>
+
+    </main>
+    <!-- end #main -->
+    <?php endif;?>
+
+    <?php endif; //end of have_posts() ?>
 
     <?php get_sidebar(); ?>
 
-  </div> <!-- end #inner-content -->
+  </div>
+  <!-- end #inner-content -->
 
-</div> <!-- end #content -->
+</div>
 
 <?php get_footer(); ?>
--- parts/loop-slider.php
+++ parts/loop-slider.php
@@ -1,19 +1,10 @@
-
 <div class="orbit" role="region" aria-label="Featured Post" data-orbit>
   <ul class="orbit-container">
-    <!-- <button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
-    <button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button> -->
-    <?php
-      $postslist = get_posts('numberposts=1&order=DESC&orderby=date');
-      foreach ($postslist as $post) :
-      setup_postdata($post);
-    ?>
     <li class="orbit-slide">
       <a href="<?php the_permalink() ?>"><?php the_post_thumbnail('full', array( 'class'	=> "orbit-image")); ?></a>
       <figcaption class="orbit-caption large-5 columns large-push-7">
         <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
       </figcaption>
     </li>
-    <?php endforeach; ?>
   </ul>
 </div>
